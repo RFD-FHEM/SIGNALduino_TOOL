87229894412ad79381d082763660f3bbb6c30a9e
sidey79 <7968127+sidey79@users.noreply.github.com>
2019-05-05 00:22:38 +0200

    Update build_controls_list.sh
    Include .json instead of .hash files in controls file

--- build_controls_list.sh
+++ build_controls_list.sh
@@ -7,7 +7,7 @@ do
     FILESIZE=$(stat -c%s "$FILE")
 	FILE=$(echo "$FILE"  | cut -c 3-)
 	printf "UPD %s %-7d %s\n" "$TIME" "$FILESIZE" "$FILE"  >> controls_SD_TOOL.txt
-done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" o -name "*.hash"\) -print0 | sort -z -g)
+done <   <(find ./FHEM -maxdepth 2 \( -name "*.pm" -o -name "*.txt" o -name "*.json"\) -print0 | sort -z -g)
 
 
 
